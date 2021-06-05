eeb556bb02c72ff84c21d4a13ba0bd46f5760666
Adam Josefus <adamjosefus@gmail.com>
2021-06-05 23:09:28 +0200

    Upgrade

--- lib/EditorEvent.d.ts
+++
@@ -1,8 +0,0 @@
-declare const enum EventType {
-    ExportRequest = "templateEditor-exportRequest"
-}
-export declare class EditorEvent extends CustomEvent<undefined> {
-    static ExportRequest: EventType;
-    constructor(typeArg: EventType);
-}
-export {};
