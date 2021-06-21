bb4a1afbc059c5c36240d63bb94ab2b57eddc14f
Adam Josefus <adamjosefus@gmail.com>
2021-06-21 08:07:43 +0200

    Update editor events

--- lib/EditorEvent.d.ts
+++ lib/EditorEvent.d.ts
@@ -1,8 +1,10 @@
 declare const enum EventType {
-    ExportRequest = "templateEditor-exportRequest"
+    ExportRequest = "templateEditor-exportRequest",
+    GetEditorLinkRequest = "templateEditor-getEditorLink"
 }
 export declare class EditorEvent extends CustomEvent<undefined> {
-    static readonly ExportRequest = EventType.ExportRequest;
+    static ExportRequest: EventType;
+    static GetEditorLinkRequest: EventType;
     constructor(typeArg: EventType);
 }
 export {};
--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,6 +1,7 @@
 export type { IData as ITemplateData } from "./IData.js";
 export type { ConfigType as TemplateConfigType } from "./ConfigType.js";
 export type { ExportDataType as TemplateExportDataType } from "./ExportDataType.js";
+export { EditorEvent as TemplateEditorEvent } from "./EditorEvent.js";
 export { ControllerElement as TemplateControllerElement } from "./ControllerElement.js";
 export { SceneElement as TemplateSceneElement } from "./SceneElement.js";
 export { ControllerEvent as TemplateControllerEvent } from "./ControllerEvent.js";
