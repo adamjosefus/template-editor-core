6b716f89b36a9e44b16df5d0ace37258e6bcb966
Adam Josefus <adamjosefus@gmail.com>
2021-07-07 11:26:07 +0200

    Update

--- src/ExportDataType.ts
+++ src/ExportDataType.ts
@@ -1,6 +1,14 @@
 export type ExportDataType = {
     items: Array<{
+        /**
+         * @deprecated Use 'name' porperty
+         */
         filename: string,
+        /**
+         * @deprecated Use 'file' porperty
+         */
         dataURL: string,
+        string: string,
+        file: File,
     }>
 }
